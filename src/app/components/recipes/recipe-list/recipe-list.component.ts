7f5e9ad15e750db0323acfdef6688acc6d7eef44
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-04-12 08:31:56 -0300

    Implementação do recipe-item

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [
+  recipesArray: Recipe[] = [
     new Recipe('New Recipe Test 1', 'Test recipe 1', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'),
     new Recipe('New Recipe Test 2', 'Test recipe 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
   ];
