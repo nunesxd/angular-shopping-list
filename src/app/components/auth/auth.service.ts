7eb6f4e88371b19c6edfada34d4b1a348dc1b26f
BHNB <rjnunes.prestserv@petrobras.com.br>
2023-11-27 17:04:46 -0300

    Configuração do forms de autenticaçao e do seu serviço de envio para o endpoint

---
+++ src/app/components/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string,
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0DrWEczacJb1xanwKJ3sS_UyMyfbEd5c', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
+    }
+}
