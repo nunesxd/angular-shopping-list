b5f62b245ca027c2d839b2ba8018368925fd2468
BHNB <rjnunes.prestserv@petrobras.com.br>
2023-11-28 10:06:51 -0300

    Implementação do login e alteração do subscribe dos observables do auth

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+
+import { AuthResponseData, AuthService } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-auth',
@@ -24,21 +26,25 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if(this.isLoginMode) {
-            // TODO
+            authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(
-                response => {
-                    console.log(response);
-                    this.isLoading = false;
-                },
-                errorMessage => {
-                    this.error = errorMessage;
-                    this.isLoading = false;
-                }
-            );
+            authObs = this.authService.signup(email, password);
         }
+
+        authObs.subscribe(
+            response => {
+                console.log(response);
+                this.isLoading = false;
+            },
+            errorMessage => {
+                this.error = errorMessage;
+                this.isLoading = false;
+            }
+        );
 
         form.reset()
     }
--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string,
     email: string,
     refreshToken: string,
     expiresIn: string,
     localId: string,
+    registered?: boolean
 }
 
 @Injectable({
@@ -27,5 +28,14 @@ export class AuthService {
         .pipe(catchError(errorRes => {
             return throwError(errorRes.message);
         }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD0DrWEczacJb1xanwKJ3sS_UyMyfbEd5c', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
     }
 }
