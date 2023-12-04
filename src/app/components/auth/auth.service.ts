3c851da2b356d388662ae86663f3e71850f91eb5
BHNB <rjnunes.prestserv@petrobras.com.br>
2023-12-04 11:24:49 -0300

    Implementação do autologin

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -53,6 +53,24 @@ export class AuthService {
         );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: Date,
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData) {
+            return
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        
+        if(loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -66,5 +84,6 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + Number(expiresIn) * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 }
