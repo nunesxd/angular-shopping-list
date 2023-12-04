bd15268d03a04db182ca88fd46ab26559dc07631
BHNB <rjnunes.prestserv@petrobras.com.br>
2023-12-04 11:40:44 -0300

    Implementação do autologout

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -20,6 +20,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
 
     constructor(private http: HttpClient, private router: Router) {}
 
@@ -68,12 +69,26 @@ export class AuthService {
         
         if(loadedUser.token) {
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration * 1000);
         }
     }
 
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        // Se clicarmos em logout, sem precisar passar pelo tempo de logout automático:
+        if(this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer);
+        };
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleError(errorRes: HttpErrorResponse) {
@@ -84,6 +99,7 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + Number(expiresIn) * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 }
