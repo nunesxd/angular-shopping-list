00f1ede85f52a7db03698ed7f36a2fa723c0b278
BHNB <rjnunes.prestserv@petrobras.com.br>
2023-12-05 16:22:17 -0300

    Finalização da implementação do dynamic component do alert, abordagem pelo código

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 
 import { AuthResponseData, AuthService } from "./auth.service";
 import { AlertComponent } from "../shared/alert/alert.component";
@@ -11,11 +11,12 @@ import { PlaceholderDirective } from "../shared/placeholder/placeholder.directiv
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     isLoginMode: boolean = true;
     isLoading: boolean = false;
     error: string = null;
     @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+    private closeSub: Subscription;
 
     constructor(private authService: AuthService, private router: Router) {}
 
@@ -59,11 +60,24 @@ export class AuthComponent {
         this.error = null;
     }
 
+    ngOnDestroy(): void {
+        if(this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
+    }
+
     // Abordagem programática do alert dinâmico:
     private showErrorAlert(errorMessage: string) {
         const hostViewContainerRef = this.alertHost.viewContainerRef;
         
+        // Remove qualquer outro elemento Angular que esteja inserido:
         hostViewContainerRef.clear();
-        hostViewContainerRef.createComponent<AlertComponent>(AlertComponent)
+        const componentRef = hostViewContainerRef.createComponent<AlertComponent>(AlertComponent);
+
+        componentRef.instance.message = errorMessage;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
     }
 }
